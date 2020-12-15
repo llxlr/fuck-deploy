a6708aa047ad8342a90629d0e6e8a0b328db500a
james yang <yanghongday369@gmail.com>
2020-12-15 23:31:03 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh -l
 
-echo "$"
-echo "INPUT_A: $INPUT_A"
+echo "type: $INPUT_TYPE"
+echo "username: $INPUT_USERNAME"
+echo "host: $INPUT_HOST"
+echo "port: $INPUT_PORT"
+echo "password: $INPUT_PASSWORD"
+echo "private_key: $INPUT_PRIVATE_KEY"
+echo "remote_path: $INPUT_REMOTE_PATH"
+echo "local_path: $INPUT_LOCAL_PATH"
+echo "include: $INPUT_INCLUDE"
+echo "exclude: $INPUT_EXCLUDE"
+echo "args: $INPUT_ARGS"
