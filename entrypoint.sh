abf332017e2f2c7095d1a53738d932acb0766f09
james yang <yanghongday369@gmail.com>
2020-12-16 01:19:21 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,22 @@
 #!/bin/sh -l
 set -eu
 
-#echo "type: $INPUT_TYPE"
-#echo "username: $INPUT_USERNAME"
-#echo "host: $INPUT_HOST"
-#echo "port: $INPUT_PORT"
-#echo "password: $INPUT_PASSWORD"
-#echo "private_key: $INPUT_PRIVATE_KEY"
-#echo "remote_path: $INPUT_REMOTE_PATH"
-#echo "local_path: $INPUT_LOCAL_PATH"
-#echo "include: $INPUT_INCLUDE"
-#echo "exclude: $INPUT_EXCLUDE"
-#echo "args: $INPUT_ARGS"
-
 if [ -z "$INPUT_TYPE" ]; then
   echo 'Required Args parameter'
   exit 1
 fi
 
-deploy_args="$INPUT_USERNAME@$INPUT_HOST:$INPUT_REMOTE_PATH"
+deploy_args="$INPUT_USERNAME@$INPUT_HOST:$INPUT_REMOTE_PATH" 
+include=(${INPUT_INCLUDE//,/})
+for var in ${include[@]}
+do
+  echo $var >> include.txt
+done
+exclude=(${INPUT_EXCLUDE//,/})
+for var in ${exclude[@]}
+do
+  echo $var >> exclude.txt
+done
 
 if [[ $INPUT_TYPE == "private_key" ]]
 then
