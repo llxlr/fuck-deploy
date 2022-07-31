e0d582e73a0a60763e692132efabc1a13d78d24f
llxlr <i@xhlr.top>
2022-07-31 15:26:14 +0800

    chore: update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
 set -eu
 
 if [ -z "$INPUT_TYPE" ]
@@ -30,29 +30,29 @@ done
 echo $INCLUDE
 echo $EXCLUDE
 
-# if [[ $INPUT_TYPE == "key" ]]
-# then
-#   SSHPATH="$HOME/.ssh"
-#   mkdir -p $SSHPATH
-#   echo "$INPUT_KEY" > "$SSHPATH/id_rsa.pem"
-#   chmod 700 $SSHPATH
-#   chmod 600 "$SSHPATH/id_rsa.pem"
-#   sh -c "rsync $INPUT_ARGS -e 'ssh -i $path/id_rsa.pem -p $INPUT_PORT $INPUT_SSH_ARGS -l $INPUT_USERNAME' $INCLUDE $EXCLUDE $LOCAL_PATH $INPUT_USERNAME@$INPUT_HOST:$REMOTE_PATH"
+if [[ $INPUT_TYPE == "key" ]]
+then
+  SSHPATH="$HOME/.ssh"
+  mkdir -p $SSHPATH
+  echo "$INPUT_KEY" > "$SSHPATH/id_rsa.pem"
+  chmod 700 $SSHPATH
+  chmod 600 "$SSHPATH/id_rsa.pem"
+  rsync $INPUT_ARGS -e "ssh -i $path/id_rsa.pem -p $INPUT_PORT $INPUT_SSH_ARGS -l $INPUT_USERNAME" $INCLUDE $EXCLUDE $LOCAL_PATH $INPUT_USERNAME@$INPUT_HOST:$REMOTE_PATH
 
-#   if [[ -z $INPUT_SCRIPTS ]]
-#   then
-#     sh -c "ssh -i $path/id_rsa.pem -p $INPUT_PORT $INPUT_SSH_ARGS -l $INPUT_USERNAME bash -s < $INPUT_SCRIPTS $ENVIRONMENT"
-#   if
-# elif [[ $INPUT_TYPE == "password" ]]
-# then
-#   export SSHPASS=$INPUT_PASSWORD
-#   sh -c "rsync $INPUT_ARGS -e 'sshpass -e ssh -p $INPUT_PORT $INPUT_SSH_ARGS -l $INPUT_USERNAME' $INCLUDE $EXCLUDE $LOCAL_PATH $INPUT_USERNAME@$INPUT_HOST:$REMOTE_PATH"
+  if [[ -z $INPUT_SCRIPTS ]]
+  then
+    ssh -i $path/id_rsa.pem -p $INPUT_PORT $INPUT_SSH_ARGS -l $INPUT_USERNAME bash -s < $INPUT_SCRIPTS $ENVIRONMENT
+  if
+elif [[ $INPUT_TYPE == "password" ]]
+then
+  export SSHPASS=$INPUT_PASSWORD
+  rsync $INPUT_ARGS -e "sshpass -e ssh -p $INPUT_PORT $INPUT_SSH_ARGS -l $INPUT_USERNAME" $INCLUDE $EXCLUDE $LOCAL_PATH $INPUT_USERNAME@$INPUT_HOST:$REMOTE_PATH
 
-#   if [[ -z $INPUT_SCRIPTS ]]
-#   then
-#     sh -c "sshpass -e ssh -p $INPUT_PORT $INPUT_SSH_ARGS -l $INPUT_USERNAME bash -s < $INPUT_SCRIPTS $ENVIRONMENT"
-#   if
-# else
-#   echo "Parameter error !"
-#   exit 1
-# fi
+  if [[ -z $INPUT_SCRIPTS ]]
+  then
+    sshpass -e ssh -p $INPUT_PORT $INPUT_SSH_ARGS -l $INPUT_USERNAME bash -s < $INPUT_SCRIPTS $ENVIRONMENT
+  if
+else
+  echo "Parameter error !"
+  exit 1
+fi
