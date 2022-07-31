116969487a1214f453643adbe185957f778cf17f
llxlr <i@xhlr.top>
2022-07-31 15:14:12 +0800

    chore: update

--- README.md
+++ README.md
@@ -3,17 +3,17 @@ Use rsync with SSH and SSHPASS to deploy project in server
 
 ## Usage
 
-  1. `type`: login verification method
-   - `port`: default `22`
-   - `password`: Password
-  2. `username`: server username
-  3. `host`: ip address
-  4. `private_key`: private key
-  5. `remote_path`: server absolute path
-  6. `local_path`: Github Actions' server relative path
-  7. `args`: rsync args
-  8. `ssh_args`: ssh args
-  9. `script`: support script
+  1. `type`: Authentication Method
+   - `key`: Private Key
+   - `password`: Remote Server Password
+  2. `username`: Remote Server Username
+  3. `host`: Remote Server IP Address or Domain Name
+  4. `port`: Remote Server Port, default `22`
+  5. `remote_path`: Remote Server Path
+  6. `local_path`: Github Runner Local Relative Path
+  7. `args`: Rsync Arguments
+  8. `ssh_args`: SSH Arguments
+  9. `script`: Deployment Script
 
 Environment: `prod` or `dev`
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/sh -l
 set -eu
 
-if [[ -z "$INPUT_TYPE" ]]
+if [ -z "$INPUT_TYPE" ]
 then
-  echo "Required Args parameter !"
+  echo "Required args parameter !"
   exit 1
 fi
 
-echo $ENVIRONMENT
-echo $INPUT_SCRIPTS
 echo $INPUT_INCLUDE
 echo $INPUT_EXCLUDE
+echo $INPUT_SCRIPTS
+echo $ENVIRONMENT
 
 LOCAL_PATH="$GITHUB_WORKSPACE/$INPUT_LOCAL_PATH"
-REMOTE_PATH="$INPUT_REMOTE_PATH" 
+REMOTE_PATH="$INPUT_REMOTE_PATH"
 
 INCLUDE=""
 for i in ${INPUT_INCLUDE[@]}
@@ -24,7 +24,7 @@ done
 EXCLUDE=""
 for i in ${INPUT_EXCLUDE[@]}
 do
-  EXCLUDE="${EXCLUDE}--include ${i} "
+  EXCLUDE="${EXCLUDE}--exclude ${i} "
 done
 
 echo $INCLUDE
@@ -34,14 +34,14 @@ echo $EXCLUDE
 # then
 #   SSHPATH="$HOME/.ssh"
 #   mkdir -p $SSHPATH
-#   chmod 
 #   echo "$INPUT_KEY" > "$SSHPATH/id_rsa.pem"
+#   chmod 700 $SSHPATH
 #   chmod 600 "$SSHPATH/id_rsa.pem"
 #   sh -c "rsync $INPUT_ARGS -e 'ssh -i $path/id_rsa.pem -p $INPUT_PORT $INPUT_SSH_ARGS -l $INPUT_USERNAME' $INCLUDE $EXCLUDE $LOCAL_PATH $INPUT_USERNAME@$INPUT_HOST:$REMOTE_PATH"
 
 #   if [[ -z $INPUT_SCRIPTS ]]
 #   then
-#     sh -c "ssh -i $path/id_rsa.pem -p $INPUT_PORT $INPUT_SSH_ARGS -l $INPUT_USERNAME bash -s < $ENVIRONMENT"
+#     sh -c "ssh -i $path/id_rsa.pem -p $INPUT_PORT $INPUT_SSH_ARGS -l $INPUT_USERNAME bash -s < $INPUT_SCRIPTS $ENVIRONMENT"
 #   if
 # elif [[ $INPUT_TYPE == "password" ]]
 # then
@@ -50,7 +50,7 @@ echo $EXCLUDE
 
 #   if [[ -z $INPUT_SCRIPTS ]]
 #   then
-#     sh -c "sshpass -e ssh -p $INPUT_PORT $INPUT_SSH_ARGS -l $INPUT_USERNAME bash -s < $ENVIRONMENT"
+#     sh -c "sshpass -e ssh -p $INPUT_PORT $INPUT_SSH_ARGS -l $INPUT_USERNAME bash -s < $INPUT_SCRIPTS $ENVIRONMENT"
 #   if
 # else
 #   echo "Parameter error !"
